9c1e6dad7866fa800f8b1d86e81c19ff6d214ad0
msantl <msantl.ck@gmail.com>
2014-12-11 13:41:45 +0100

    update routing info

--- odl/mystats/src/main/java/com/example/mystats/MyStats.java
+++ odl/mystats/src/main/java/com/example/mystats/MyStats.java
@@ -1,5 +1,6 @@
 package com.example.mystats;
 
+import java.util.Set;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
@@ -8,11 +9,14 @@ import java.util.HashMap;
 import org.opendaylight.controller.sal.utils.ServiceHelper;
 import org.opendaylight.controller.sal.core.Node;
 import org.opendaylight.controller.sal.core.NodeConnector;
+import org.opendaylight.controller.sal.core.Edge;
+import org.opendaylight.controller.sal.core.Property;
+import org.opendaylight.controller.sal.core.Host;
 import org.opendaylight.controller.sal.reader.NodeConnectorStatistics;
 import org.opendaylight.controller.statisticsmanager.IStatisticsManager;
 import org.opendaylight.controller.switchmanager.ISwitchManager;
+import org.opendaylight.controller.topologymanager.ITopologyManager;
 import org.opendaylight.controller.switchmanager.Switch;
-import org.opendaylight.controller.sal.core.Property;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,6 +47,7 @@ public class MyStats{
 
     public class Data {
         private Node node;
+        private List<Host> host;
         private NodeConnector nc;
         private long pkt_drop;
         private long pkt_sent;
@@ -67,12 +72,19 @@ public class MyStats{
         public void setPacketDrop(long pkt_drop) {this.pkt_drop = pkt_drop;}
         public void setPacketSent(long pkt_sent) {this.pkt_sent = pkt_sent;}
         public void setBandwidth(String bandwidth) {this.bandwidth = bandwidth;}
+        public void setHosts(List<Host> hosts) {
+            this.host = new ArrayList();
+            for (Host h : hosts) {
+                this.host.add(h);
+            }
+        }
 
         public Node getNode() {return this.node;}
         public NodeConnector getNodeConnector() {return this.nc;}
         public long getPacketDrop() {return this.pkt_drop;}
         public long getPacketSent() {return this.pkt_sent;}
         public String getBandwidth() {return this.bandwidth;}
+        public List<Host> getHosts() {return this.host;}
     }
 
     void getFlowStatistics() {
@@ -87,9 +99,13 @@ public class MyStats{
         ISwitchManager switchManager = (ISwitchManager) ServiceHelper
             .getInstance(ISwitchManager.class, containerName, this);
 
+        ITopologyManager topologyManager = (ITopologyManager) ServiceHelper
+            .getInstance(ITopologyManager.class, containerName, this);
+
         List<Switch> switches =
             switchManager.getNetworkDevices();
 
+        // get statistics and bandwidth property
         for (Switch swc : switches) {
             Node node = swc.getNode();
             List<NodeConnectorStatistics> stat = statsManager
@@ -136,10 +152,58 @@ public class MyStats{
 
         }
 
+        // get topology information
+        Map<Node,Set<Edge>> topology = topologyManager.getNodeEdges();
+
+        for (Node key : edge.keySet()) {
+
+            for (Edge e : topology.get(key)) {
+
+                NodeConnector tail_nc = e.getTailNodeConnector();
+                NodeConnector head_nc = e.getHeadNodeConnector();
+
+                Node tail = tail_nc.getNode();
+                Node head = head_nc.getNode();
+
+                // head -> tail
+                List<Data> neighbours = edge.get(head);
+                for (Data n : neighbours) {
+                    if (head_nc.equals(n.getNodeConnector())) {
+                        n.setNode(tail);
+                    }
+                }
+
+            }
+        }
+
+        // update host info
+        for (Node key : edge.keySet()) {
+            for (Data n : edge.get(key)) {
+                List<Host> hosts = topologyManager
+                    .getHostsAttachedToNodeConnector(n.getNodeConnector());
+
+                if (hosts != null) {
+                    n.setHosts(hosts);
+                }
+            }
+        }
+
+        // print info
         for (Node key : edge.keySet()) {
             System.out.println("Switch: " + key.toString());
 
             for (Data data : edge.get(key)) {
+                if (data.getNode() != null) {
+                    System.out.println("\tNode: " + data.getNode().toString());
+                }
+                if (data.getHosts() != null) {
+                    System.out.print("\tHosts: ");
+                    for (Host h : data.getHosts()) {
+                        System.out.print(h.getNetworkAddressAsString() + ", ");
+                    }
+                    System.out.println("");
+                }
+
                 System.out.println("\tNodeConnector: " +
                         data.getNodeConnector().getNodeConnectorIdAsString());
                 System.out.println("\tPacketDrop: " + data.getPacketDrop());
